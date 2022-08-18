239ad88c681a779281d6cf35ecbf5d63244c39c3
michael <michael.srempf@gmail.com>
2022-08-18 16:37:49 +0200

    gradle update

--- app/src/main/java/warehouse/assistant/MainActivity.kt
+++ app/src/main/java/warehouse/assistant/MainActivity.kt
@@ -1,16 +1,24 @@
 package warehouse.assistant
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import warehouse.assistant.ui.theme.WarehouseAssistantTheme
+import warehouse.assistant.ui.theme.Yellow40
+import java.time.LocalDateTime
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +28,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.surface
                 ) {
                     Greeting("Android")
                 }
@@ -31,13 +39,34 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = "${LocalDateTime.now().toString()}", fontSize = 24.sp)
+
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true,
+        uiMode = UI_MODE_NIGHT_YES
+)
+@Composable
+fun DefaultNightPreview() {
+    WarehouseAssistantTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+@Preview(showBackground = true,
+)
 @Composable
 fun DefaultPreview() {
     WarehouseAssistantTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.primary
+        ) {
+            Greeting("Android")
+        }
     }
 }
